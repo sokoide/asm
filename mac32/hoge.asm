f449b57b9cd9d6a94a28f13fbe2c427c28dcbe37
So Koide <sokoide@gmail.com>
2019-05-21 22:26:23 +0900

    Added .gitignore. Added document for additional 4bytes in 32bit version.

--- mac32/hoge.asm
+++ mac32/hoge.asm
@@ -13,14 +13,33 @@ _main:
     push  dword 1               ; fd=1=stdout
 
     mov   eax, 4    ; 'write' syscall number 4
-    sub   esp, 4    ; stack must be 16 byte aligned (3 pushed dwords=12 bytes, needs 4 more bytes)
+
+    ; http://takesako.hatenablog.com/entry/20090313/1236937017
+    ;  dummy 4byte push for return address.
+    ; because syscall is generally called by 'call' but we call it by int 0x80.
+    ; if you want to use 'call', it'll be as below.
+    ;
+    ;kernel:
+    ; 	int	80h	; Call kernel
+    ; 	ret
+
+    ; open:
+    ; 	push	dword mode
+    ; 	push	dword flags
+    ; 	push	dword path
+    ; 	mov	eax, 5
+    ; 	call	kernel
+    ; 	add	esp, byte 12
+    ; 	ret
+    ;
+    sub   esp, 4    ;
     int   0x80      ; syscall
     add   esp, 16   ; reset stack
 
 
     push  dword 0   ; exit code 0
     mov   eax, 1    ; 'exit' syscall number 1
-    sub   esp, 4    ; align stack, but why 12 fails (16 byte align)?
+    sub   esp, 4    ; dummy 4byte push
     int   0x80      ; syscall
 
 section .data
